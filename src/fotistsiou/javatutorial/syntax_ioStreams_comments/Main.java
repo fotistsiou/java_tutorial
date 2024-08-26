04b646b9a573741d9f1f1db805a0a5c06ddce916
fotistsiou <fotis.tsioumas@outlook.com>
2024-08-26 18:50:11 +0300

    Syntax - IO Streams - Comments

---
+++ src/fotistsiou/javatutorial/syntax_ioStreams_comments/Main.java
@@ -0,0 +1,24 @@
+package fotistsiou.javatutorial.syntax_ioStreams_comments;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /* IO Streams - Output */
+        System.out.print("Hello! My name is Fotis and I'm ");
+        System.out.print(2024 - 1995);
+        System.out.println(" years old.");
+
+        /* IO Streams - Input */
+        // 1. Create a Scanner object
+        Scanner scanner = new Scanner(System.in);
+        // 2. Add question
+        System.out.println("What is your name?");
+        // 3. Read/Save user input
+        String name = scanner.nextLine(); // read/save String
+        System.out.println("How old are you?");
+        int age = scanner.nextInt(); // read/save int
+        // 4. Print user input
+        System.out.println("Your name is " + name + " and you're " + age + " years old.");
+    }
+}
