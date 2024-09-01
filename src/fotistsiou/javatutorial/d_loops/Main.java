c0f412d3731189c0b2e847b47cefed81a050b1c5
fotistsiou <fotis.tsioumas@outlook.com>
2024-09-01 16:16:48 +0300

    Loops

--- src/fotistsiou/javatutorial/d_loops/Main.java
+++ src/fotistsiou/javatutorial/d_loops/Main.java
@@ -7,10 +7,11 @@ public class Main {
         /* Loops */
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("----- Calculate Multiples of Number -----");
 
         // While Loop
-        System.out.println("----- Calculate Multiples of Number -----");
-        boolean input = false;
+        // Checking if the condition is true, before execute the code block.
+        boolean input = false; // I need to assign a value to 'input' variable outside the code block.
         while (!input) {
             System.out.println("Enter an integer N from 2 to 20: ");
             int n = scanner.nextInt();
@@ -28,6 +29,24 @@ public class Main {
         }
 
         // Do While Loop
+        // Will execute the code block once, before checking if the condition is true.
+        boolean input2; // I can assign a value to 'input2' variable inside the code block.
+        do {
+            System.out.println("Enter an integer from 1 to 10: ");
+            int n = scanner.nextInt();
+            if (n >= 1 && n <= 10) {
+                int i = 1;
+                while (i <= 10) {
+                    int multiple = i * n;
+                    System.out.println(n + " x " + i + " = " + multiple);
+                    i++;
+                }
+                input2 = true;
+            } else {
+                System.out.println("Wrong Input! Try again!");
+                input2 = false;
+            }
+        } while (!input2);
 
         // For Loop
 
