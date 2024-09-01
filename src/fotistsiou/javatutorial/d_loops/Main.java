6fd1f60e0dcc9e8d09e0f4fe86e6729cfc9ae491
fotistsiou <fotis.tsioumas@outlook.com>
2024-09-01 19:25:58 +0300

    Loops

--- src/fotistsiou/javatutorial/d_loops/Main.java
+++ src/fotistsiou/javatutorial/d_loops/Main.java
@@ -66,6 +66,20 @@ public class Main {
         }
 
         // Break / Continue
+        // The break statement can be used to jump out of a loop.
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
+        // The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
 
