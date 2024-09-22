7fba12710a068e766d6c073a7685c4be0889490b
fotistsiou <fotis.tsioumas@outlook.com>
2024-09-22 16:03:45 +0300

    Records

--- src/fotistsiou/javatutorial/fundamentals/g_oop/Main.java
+++ src/fotistsiou/javatutorial/fundamentals/g_oop/Main.java
@@ -43,5 +43,27 @@ public class Main {
         // -> so the "Student" object can be passed as an argument to the "acceptInCourse" method.
         professor.acceptInCourse(student1);
         professor.acceptInCourse(student2);
+
+
+        /* "Student" record */
+
+        // Create records objects
+        RStudent rStudent1 = new RStudent("Rafailia Tsiouma", "Stud0004");
+        RStudent rStudent2 = new RStudent("Christina Sideri", "Stud0005");
+        // Use the auto-generated "toString" method
+        System.out.println("I am a 'RStudent' record object -> " + rStudent1.toString());
+        System.out.println("I am a 'RStudent' record object -> " + rStudent2.toString());
+        // Use the auto-generated "equals" method
+        if (rStudent1.equals(rStudent2)) {
+            System.out.println("The 'RStudent' record objects are the same.");
+        } else {
+            System.out.println("The 'RStudent' record objects are different.");
+        }
+        // Use the auto-generated "hashCode" method
+        if (rStudent1.hashCode() == rStudent2.hashCode()) {
+           System.out.println("The 'RStudent' record objects are the same.");
+        } else {
+           System.out.println("The 'RStudent' record objects are different.");
+        }
     }
 }
