58835e29cefd41cbb68c4de70e7cb6dbf6d0b97f
fotistsiou <fotis.tsioumas@outlook.com>
2024-10-06 15:22:33 +0300

    Conditional Statements

--- src/fotistsiou/javatutorial/fundamentals/f_conditionalStatements/ConditionalStatements.java
+++ src/fotistsiou/javatutorial/fundamentals/f_conditionalStatements/ConditionalStatements.java
@@ -4,11 +4,11 @@ import java.util.Locale;
 import java.util.Scanner;
 
 /* Conditional Statements */
-// Java has the following conditional statements:
-// - Use if to specify a block of code to be executed, if a specified condition is true
-// - Use else to specify a block of code to be executed, if the same condition is false
-// - Use else if to specify a new condition to test, if the first condition is false
-// - Use switch to specify many alternative blocks of code to be executed
+// -> Java has the following conditional statements:
+//    - Use if to specify a block of code to be executed, if a specified condition is true
+//    - Use else to specify a block of code to be executed, if the same condition is false
+//    - Use else if to specify a new condition to test, if the first condition is false
+//    - Use switch to specify many alternative blocks of code to be executed
 
 public class ConditionalStatements {
 
