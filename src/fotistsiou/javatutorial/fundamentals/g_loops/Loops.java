7de897dff9bc494a9cdbe0a9194734360088d120
fotistsiou <fotis.tsioumas@outlook.com>
2024-10-11 10:03:28 +0300

    Loops

--- src/fotistsiou/javatutorial/fundamentals/g_loops/Loops.java
+++ src/fotistsiou/javatutorial/fundamentals/g_loops/Loops.java
@@ -20,9 +20,10 @@ public class Loops {
         System.out.println("----- Calculate Multiples of Number -----");
 
         // While Loop
-        boolean input = false; // I need to assign a value to 'input' variable outside the code block.
+        // -> Exercise 1: Calculate multiples of number
+        boolean input = false;
         while (!input) {
-            System.out.println("Enter an integer from 2 to 20: ");
+            System.out.println("Enter an integer from 1 to 10: ");
             int n = scanner.nextInt();
             if (n >= 2 && n <= 20) {
                 int i = 1;
@@ -36,8 +37,21 @@ public class Loops {
                 System.out.println("Wrong Input! Try again!");
             }
         }
+        // -> Exercise 2: Write a Java program that reads a sequence of temperature readings in degrees Celsius
+        //                until a reading of 100 degrees or more is encountered.
+        int maxTemperature = 0;
+        System.out.println("Enter a temperature: ");
+        int temperature = scanner.nextInt();
+        while (temperature < 100) {
+            maxTemperature = Math.max(maxTemperature, temperature);
+            System.out.println("Enter the next temperature: ");
+            temperature = scanner.nextInt();
+        }
+        System.out.println("Max Temperature: " + maxTemperature);
+
 
         // Do While Loop
+        // -> Exercise 1: Calculate multiples of number
         boolean input2; // I can assign a value to 'input2' variable inside the code block.
         do {
             System.out.println("Enter an integer from 1 to 10: ");
@@ -55,8 +69,18 @@ public class Loops {
                 input2 = false;
             }
         } while (!input2);
+        // -> Exercise 2: Find the sum of all elements of a sequence, ending with the number 0.
+        int sum = 0;
+        int number;
+        do {
+            System.out.println("Enter a number: ");
+            number = scanner.nextInt();
+            sum += number;
+        } while (number > 0);
+        System.out.println("Sum of all elements of the sequence: " + sum);
 
         // For Loop
+        // -> Exercise 1: Calculate multiples of number
         boolean input3 = false; // I need to assign a value to 'input' variable outside the code block.
         while (!input3) {
             System.out.println("Enter an integer from 2 to 20: ");
@@ -71,6 +95,15 @@ public class Loops {
                 System.out.println("Wrong Input! Try again!");
             }
         }
+        // Exercise 2: Print the sum of all integers from a to b including both.
+        System.out.println("Enter 2 integers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int sumOfInts = 0;
+        for (int i = num1; i <= num2; i++) {
+            sumOfInts += i;
+        }
+        System.out.println(sumOfInts);
 
         // Break Statement
         for (int i = 0; i < 10; i++) {
