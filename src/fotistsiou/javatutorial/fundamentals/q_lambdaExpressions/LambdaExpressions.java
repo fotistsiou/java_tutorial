f3371a21ae3941797e64ea871ec0ccd618c72848
fotistsiou <fotis.tsioumas@outlook.com>
2024-10-19 16:15:20 +0300

    Lambda Expressions

---
+++ src/fotistsiou/javatutorial/fundamentals/q_lambdaExpressions/LambdaExpressions.java
@@ -0,0 +1,10 @@
+package fotistsiou.javatutorial.fundamentals.q_lambdaExpressions;
+
+/* Lambda Expressions */
+
+public class LambdaExpressions {
+
+    public static void main(String[] args) {
+
+    }
+}
