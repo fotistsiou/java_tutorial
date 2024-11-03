caac89a3e36be3229041527164597a767868ab9e
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-03 15:22:01 +0200

    Notes Optimizations

--- src/fotistsiou/javatutorial/fundamentals/f_conditionalStatements/ConditionalStatements.java
+++ src/fotistsiou/javatutorial/fundamentals/f_conditionalStatements/ConditionalStatements.java
@@ -1,72 +1,77 @@
 package fotistsiou.javatutorial.fundamentals.f_conditionalStatements;
 
-import java.util.Locale;
 import java.util.Scanner;
 
-/* Conditional Statements */
-// -> Java has the following conditional statements:
-//    - Use if to specify a block of code to be executed, if a specified condition is true
-//    - Use else to specify a block of code to be executed, if the same condition is false
-//    - Use else if to specify a new condition to test, if the first condition is false
-//    - Use switch to specify many alternative blocks of code to be executed
+/**
+ * Conditional Statements
+ * -> Java has the following conditional statements:
+ *    - Use if to specify a block of code to be executed, if a specified condition is true.
+ *    - Use else to specify a block of code to be executed, if the same condition is false.
+ *    - Use else if to specify a new condition to test, if the first condition is false.
+ *    - Use switch to specify many alternative blocks of code to be executed.
+ * */
 
 public class ConditionalStatements {
 
     public static void main(String[] args) {
-        // Write a travel assistant program that:
-        // - Reads the budget, the tickets fare, the stay charge, the car fee and the number of days.
-        // - Then, calculates the leftover amount.
-        // - Finally, prints a comment about leftover amount and another comment about travel days.
-
         Scanner scanner = new Scanner(System.in);
-        Locale systemLocale = Locale.getDefault(); // Configure "Scanner" to use system's default locale format (for double separators).
-        scanner.useLocale(systemLocale);
 
-        System.out.println(("Enter your budget:"));
-        double budget = scanner.nextDouble();
-        System.out.println(("Enter the tickets fare:"));
-        double ticketsFare = scanner.nextDouble();
-        System.out.println("Enter the charge for your stay:");
-        double stayCharge = scanner.nextDouble();
-        System.out.println("Enter the car rental fee:");
-        double carFee = scanner.nextDouble();
-        System.out.println("Enter the number of days of your travel:");
-        int numOfDays = scanner.nextInt();
-
-        double totalOfExpenses = ticketsFare + stayCharge + carFee;
-        double leftOver = budget - totalOfExpenses;
-
-        System.out.println("Your budget: " + budget);
-        System.out.println("The total of expenses: " + totalOfExpenses);
-
-        // If Statement
-        if (leftOver > 0) {
-            System.out.println("Τhe money left over for the rest of the trip is " + leftOver);
-        } else if (leftOver == 0) {
-            System.out.println("You have no money left over for the rest of the trip.");
-        } else {
-            System.out.println("Your budget is less than your travel expenses.");
+        // The single if case
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        if (age > 100) {
+            System.out.println("Very experienced person");
         }
 
-        // Switch Statement
-        switch (numOfDays) {
+        // The if-else case
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("It's an even number");
+        } else {
+            System.out.println("It's an odd number");
+        }
+
+        // The if-else-if case
+        System.out.println("Enter an amount: ");
+        long dollars = scanner.nextLong();
+        if (dollars < 1000) {
+            System.out.println("Buy a laptop");
+        } else if (dollars < 2000) {
+            System.out.println("Buy a personal computer");
+        } else if (dollars < 100_000) {
+            System.out.println("Buy a server");
+        } else {
+            System.out.println("Buy a data center or a quantum computer");
+        }
+
+        // The switch case
+        System.out.println("Enter the number of current day: ");
+        int day = scanner.nextInt();
+        switch (day) {
             case 1:
-                System.out.println("Your travel is " + numOfDays + " day, so it is short.");
+                System.out.println("Monday");
                 break;
             case 2:
+                System.out.println("Tuesday");
+                break;
             case 3:
+                System.out.println("Wednesday");
+                break;
             case 4:
-                System.out.println("Your travel is " + numOfDays + " days, so it is short.");
+                System.out.println("Thursday");
                 break;
             case 5:
+                System.out.println("Friday");
+                break;
             case 6:
+                System.out.println("Saturday");
+                break;
             case 7:
-                System.out.println("Your travel is " + numOfDays + " days, so it is neither short nor far.");
+                System.out.println("Sunday");
                 break;
             default:
-                System.out.println("Your travel is " + numOfDays + " days, so it is far.");
+                System.out.println("Invalid day");
         }
-
-        scanner.close();
     }
 }
