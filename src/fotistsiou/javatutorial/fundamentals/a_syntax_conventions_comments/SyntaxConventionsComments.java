90ab550d57867fe82a6c204f9a9090958beebeeb
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-16 17:49:01 +0200

    Syntax

--- src/fotistsiou/javatutorial/fundamentals/a_syntax_conventions_comments/SyntaxConventionsComments.java
+++ src/fotistsiou/javatutorial/fundamentals/a_syntax_conventions_comments/SyntaxConventionsComments.java
@@ -5,7 +5,7 @@ package fotistsiou.javatutorial.fundamentals.a_syntax_conventions_comments;
  * -> Every Java application must have at least one class.
  * -> The class name should match the Java file name.
  * -> To execute code in Java, a class must contain a main() method.
- * -> The main() method is the starting/entry point for every Java application.
+ * -> The main() method is the entry point for every Java application.
  * -> Curly braces {...} mark the beginning and end of a block of code.
  * -> Each code statement must end with a semicolon.
  * ---------------------------------------------------------------------------------------------
