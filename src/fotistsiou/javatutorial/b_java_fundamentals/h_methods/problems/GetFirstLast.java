ff87a8e180c2be13ee80463dda6aaed9ba9584af
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-23 18:27:55 +0200

    Methods

--- src/fotistsiou/javatutorial/b_java_fundamentals/h_methods/problems/GetFirstLast.java
+++ src/fotistsiou/javatutorial/b_java_fundamentals/h_methods/problems/GetFirstLast.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * ---------------------------
  * Write a method named getFirstAndLast. The method must take an array of ints and return a new array of ints.
  * The returned array must contain two elements: the first and the last elements of the input array.
- * It is guaranteed that the input array always has at least one element.
+ * It is guaranteed that the input array always has at least one element. 
  */
 
 public class GetFirstLast {
